74d5d130bebbe64e9f29374148ca68966bddaa51
Evair Domingues <evairbd@gmail.com>
2026-01-29 11:36:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import "./styles/theme.css";
+import "./styles/global.css";
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
+
+export const App = () => {
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+            labelText="Task"
+            id='meuInput'
+            type="text"
+            placeholder="Digite algo"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <Cycles />
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+
+        </form>
+      </Container>
+    </>
+  );
+};
